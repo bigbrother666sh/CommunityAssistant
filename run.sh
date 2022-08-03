74ccf8fbbe49e112484014b38242d2df3bfaadd3
bigbrother666 <zeming.zhao@gmail.com>
2022-08-03 20:54:19 +0800

    1.07 with intent update

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 export WECHATY_PUPPET_SERVICE_NO_TLS_INSECURE_SERVER=true
 export WECHATY_PUPPET_SERVICE_ENDPOINT=127.0.0.1:8080
 export WECHATY_PUPPET_SERVICE_TOKEN=d5ef8a75-1bfb-4090-8c88-3a6c4ffb8a12
+export CACHE_DIR='.lingxiaoi'
+export accesstoken='aK0V8VO596BOVGmUeTqAujGg||YNlP4yqoXi7IjEAYuQSdGeZbF2aojWse'
 
 python ~/CommunityAssistant/run.py
